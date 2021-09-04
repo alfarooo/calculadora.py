a47ef4702ab4766e83e546f7b4949af0b67ab26d
victorleon <victorleon@respaldo.respaldo.test>
2021-09-04 00:28:29 -0500

    Complementa la funcion menu y calculadora

--- calculadora_matematica.py
+++ calculadora_matematica.py
@@ -1,4 +1,4 @@
-print ("¡Bienvenido a nuestra calculadora matematica!") 
+print ("¡Bienvenido a nuestra calculadora matematica!")
 def Menu():
 print """
 Calculadora
@@ -9,11 +9,29 @@ Menu
 3) Multiplicacion
 4) Division
 5) Salir"""
+
+
+
 def Calculadora():
 
 
 
+"""Funcion Para Calcular Operaciones Matematicas"""
+
+
+
 Menu()
+opc = int(input("Selecione Opcion\n"))
+while (opc >0 and opc <5):
+
+
+
+"""Funcion para sumar"""
+x = int(input("Ingrese Numero\n"))
+y = int(input("Ingrese Otro Numero\n"))
+if (opc==1):
+print "La Suma es:", x+y
+
 
 
 
