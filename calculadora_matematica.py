a94eaa050b102509beb74bd225de71525a5fb229
Fernando <herberthhenriquez617@gmail.com>
2021-09-03 22:52:22 -0600

    La estructura inicial del menu y la calculadora

--- calculadora_matematica.py
+++ calculadora_matematica.py
@@ -1,1 +1,20 @@
-print ("¡Bienvenido a nuestra calculadora matematica!")
+print ("¡Bienvenido a nuestra calculadora matematica!") 
+def Menu():
+print """
+Calculadora
+************
+Menu
+1) Suma
+2) Resta
+3) Multiplicacion
+4) Division
+5) Salir"""
+def Calculadora():
+
+
+
+Menu()
+
+
+
+Calculadora()
